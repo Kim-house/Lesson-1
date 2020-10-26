da5883e7ec4f9ae6113fb671b05c106eee1fbded
Kim-house <kimgakii20@gmail.com>
2020-10-26 20:31:43 +0300

    firstcommit

---
+++ lesson1.py
@@ -0,0 +1,12 @@
+x=5
+print (x)
+print (3+x)
+x=4
+print (3+x)
+print (type (x))
+
+#working with divisors
+print (5/3)
+print (5//3)
+print (5%3)
+print (x+3)
---
+++ lesson2.py
@@ -0,0 +1,12 @@
+x=4
+print ((x<6) and (x>2))
+print ((x<6) & (x>2))
+print (x>6) or (x<2)
+print ((x>6) | (x<2))
+a = [1, 2, 3]
+b = [1,2,3]
+print (a)
+print (b)
+print (a == b)
+print (1 in a)
+print (4 not in b)
