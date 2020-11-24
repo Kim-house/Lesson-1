f3af9f0713208b486a3392a6b5e3747d473082fc
Kim-house <kimgakii20@gmail.com>
2020-11-24 23:27:04 +0300

    string-manipulation

---
+++ string_manipulation.py
@@ -0,0 +1,50 @@
+fox = "tHE quICk bROWn fox"
+# print(fox.upper())
+# print(fox.lower())
+# print(fox.title())
+# print(fox.capitalize())
+# print(fox.swapcase())
+
+
+line = '         this is the content         '
+# print(line.strip())
+# print(line.lstrip())
+# print(line.rstrip())
+# print(line.center(40))
+# print(line.ljust(150))
+# print(line.rjust(280))
+
+
+num = "00000564"
+# print(num.strip('0'))
+
+# print("435".rjust(10, '0'))
+# print("435".zfill(10))
+
+L = [2, 3, 4, 3, 4, 5, 6]
+# print(L.count(3))
+
+# print(fox.count('fox'))
+
+sent = "the quick brown fox jumped over a lazy dog"
+# print(len(sent))
+# print(sent.rfind('z'))
+# print(sent.endswith('dog'))
+# print(sent.startswith('the'))
+# print(sent.replace('brown fox', 'red cat'))
+# print(sent.replace('o', '-'))
+
+# print(sent.partition('fox'))
+# print(sent.split())
+# print('--'.join(['the', 'brown', 'foxes']))
+# print('-'.join(['2', '4', '6']))
+
+pi = 3.14159
+# print('the value of pi is ' + str(pi))
+# print('The value of pi is {}'.format(pi))
+
+# print("""First letter: {0}. Last letter: {1}.""".format('a', 'z'))
+
+# print("""First letter: {first}. Last letter: {last}.""".format(first='a', last='z'))
+
+print("pi = {0:.3f}".format(pi))
