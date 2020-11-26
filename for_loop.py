62edffb1eac16096270d82dbad9cc99435695a51
Kim-house <kimgakii20@gmail.com>
2020-11-26 22:49:23 +0300

    for loop to count elements

---
+++ for_loop.py
@@ -0,0 +1,31 @@
+L = [2, 3, 4, 3, 3, 4, 5, 6]
+# print(len(L))
+
+# for a in L:
+#     if a == 3:
+#         print(a)
+
+# for a in L:
+#     C = []
+#     if a == 3:
+#         C.append(a)
+#         print(len(C))
+    
+# E = 0
+# for a in C:
+#     E = E + 1 
+# print(E)
+
+L = [2, 3, 4, 3, 3, 4, 5, 6]
+def hesabu (j, k):
+    e = 0
+    for b in j:
+        if b == k:
+            e = e + 1
+    return (e)
+print(hesabu(L, 3))
+
+e += 1
+
+
+    
--- iterators.py
+++ iterators.py
@@ -1,47 +1,47 @@
-for i in range(10):
-    print(i)
+# for i in range(10):
+#     print(i)
 
-for value in [2,3,5,7,9]:
-    print(value+1)
+# for value in [2,3,5,7,9]:
+#     print(value+1)
 
-i = iter([2, 4, 6, 8])
-print(next(i))
+# i = iter([2, 4, 6, 8])
+# print(next(i))
 
-L = [2,4,6,8,10]
-print(len(L))
-print(range(len(L)))
-for i in range(len(L)):
-    print(i, L[i])
-for i in L:
-    print(i)
+# L = [2,4,6,8,10]
+# print(len(L))
+# print(range(len(L)))
+# for i in range(len(L)):
+#     print(i, L[i])
+# for i in L:
+#     print(i)
 
-R = [3, 6, 9, 12, 15]
+# R = [3, 6, 9, 12, 15]
 # for x, y in zip (L, R):
 #     print(x, y)
-z = zip(L, R)
+# # z = zip(L, R)
 # print(*z)
 
-M, N = zip(*z)
-print(M, N)
+# M, N = zip(*z)
+# print(M, N)
 
 
-square = lambda x: x ** 2
-for z in map(square, range(10)):
-    print(z)
+# square = lambda x: x ** 2
+# for z in map(square, range(10)):
+#     print(z)
 
-is_even = lambda x: x%2==0
-for z in filter(is_even, range(10)):
-    print(z)
+# is_even = lambda x: x%2==0
+# for z in filter(is_even, range(10)):
+#     print(z)
 
 
-from itertools import permutations
-p = permutations(range(3))
-print(*p)
+# from itertools import permutations
+# p = permutations(range(3))
+# print(*p)
 
-from itertools import combinations
-c = combinations(range(4), 2)
-print(*c)
+# from itertools import combinations
+# c = combinations(range(4), 2)
+# print(*c)
 
-from itertools import product
-e = product('ab', range(3))
-print(*e)
+# from itertools import product
+# e = product('ab', range(3))
+# print(*e)
