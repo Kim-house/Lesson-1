0894764bdb0a46af4963e780b1b60328af37338e
Kim-house <kimgakii20@gmail.com>
2020-12-08 23:11:05 +0300

    data_science_web_dev_tools

---
+++ practice1.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+current_time = datetime.now()
+# my_date = dt.strptime((current_time, '%d-%m-%Y %H:%M:%S').strftime ('%m')) 
+# my_date = datetime.strptime(str(current_time),'%Y-%m-%d %H:%M:%S').strftime('%Y/%m/%d') 
+# print(my_date)
+
+print(current_time.year, current_time.month)
+
+
+# name = input("what is your name?")
+# age = int(input("what is your age "))
+# print("my name is " + name)
+# print("my age is ", age)
+
+# print("you will turn 100 in ", ((100 - age) + 2020))
+
+
+# name = str( input ('Enter your name: '))
+# age = int ( input ('Enter your age in years: '))
+# number = int ( input ('Please enter an additional number: '))
+# year = (100 - age) + 2017
+# print (('Dear Mr/Ms ' + name + ', you will turn 100 years old on ' + str ( year) + '\n' ) * number)
+
+
